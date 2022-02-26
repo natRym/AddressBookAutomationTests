fa8476a0be7bc5edc9de514b24ed86e65cc98e0d
Rymarava Natalia <rymareva.nat@gmail.com>
2022-02-26 17:38:36 +0300

    delete_the_first_group test added

--- fixture/group.py
+++ fixture/group.py
@@ -27,6 +27,15 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_group_page()
+
     def return_to_group_page(self):
         # return to group page
         wd = self.app.wd
