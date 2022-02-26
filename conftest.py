8dbbe82a837702d89b32c2ceb180c691d099c98d
Rymarava Natalia <rymareva.nat@gmail.com>
2022-02-26 22:10:04 +0300

    add tests to edit group, delete group

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ import pytest
 from fixture.application import Application
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
--- fixture/group.py
+++ fixture/group.py
@@ -27,6 +27,26 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
+    def edit_first_group(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit edition
+        wd.find_element_by_name("edit").click()
+        # enter new data
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("update").click()
+        self.return_to_group_page()
+
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
