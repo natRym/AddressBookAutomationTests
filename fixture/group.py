a5aa6b2f347b04e48a9ae6a054e05a6caf4bfcab
Rymarava Natalia <rymareva.nat@gmail.com>
2022-03-01 18:23:01 +0300

    extract separate method for add/edit group/contact. Reusing code.

--- fixture/group.py
+++ fixture/group.py
@@ -13,6 +13,13 @@ class GroupHelper:
         self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
+        self.fill_group_form(group)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_group_page()
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -23,9 +30,6 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_group_page()
 
     def edit_first_group(self, group):
         wd = self.app.wd
@@ -35,15 +39,8 @@ class GroupHelper:
         # submit edition
         wd.find_element_by_name("edit").click()
         # enter new data
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
+        # submit group edition
         wd.find_element_by_name("update").click()
         self.return_to_group_page()
 
