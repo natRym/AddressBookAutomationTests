f975f275072801edc48a573e0cd912692486fb74
Rymarava Natalia <rymareva.nat@gmail.com>
2022-03-06 11:43:31 +0300

    optimize edit using if construction

--- test/Group/test_edit_group.py
+++ test/Group/test_edit_group.py
@@ -11,3 +11,9 @@ def test_edit_first_group_to_empty_name(app):
     app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(name="", header="", footer=""))
     app.session.logout()
+
+
+def test_edit_group_name_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(name="Edit Group"))
+    app.session.logout()
