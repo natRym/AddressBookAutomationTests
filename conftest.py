e0e57d5f0fcca84c106d3b89c9ca9afd9c3a3e2c
Rymarava Natalia <rymareva.nat@gmail.com>
2022-03-07 00:36:18 +0300

    added check for session state

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ def app(request):
     global fixture
     if fixture is None:
         fixture = Application()
-        fixture.session.login(username="admin", password="secret")
+        fixture.session.ensure_login(username="admin", password="secret")
     else:
         if not fixture.is_valid():
             fixture = Application()
@@ -21,7 +21,7 @@ def app(request):
 @pytest.fixture(scope='session', autouse=True)
 def stop(request):
     def finalizer():
-        fixture.session.logout()
+        fixture.session.ensure_logout()
         fixture.destroy()
 
     request.addfinalizer(finalizer)
