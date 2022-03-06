9ca8bc767838f4cdcdf1cf2085fe015203b66867
Rymarava Natalia <rymareva.nat@gmail.com>
2022-03-06 11:46:48 +0300

    added new tests

--- test/Group/test_edit_group.py
+++ test/Group/test_edit_group.py
@@ -15,5 +15,17 @@ def test_edit_first_group_to_empty_name(app):
 
 def test_edit_group_name_first_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="Edit Group"))
+    app.group.edit_first_group(Group(name="Edit ONLY Group name"))
+    app.session.logout()
+
+
+def test_edit_group_header_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(header="Edit ONLY Group Header"))
+    app.session.logout()
+
+
+def test_edit_group_footer_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(footer="Edit ONLY Group Footer"))
     app.session.logout()
