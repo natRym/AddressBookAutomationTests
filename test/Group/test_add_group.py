55292722705736f86139e4705b31dfcb38b03547
Rymarava Natalia <rymareva.nat@gmail.com>
2022-03-13 23:12:55 +0300

    added comparison groups list

--- test/Group/test_add_group.py
+++ test/Group/test_add_group.py
@@ -3,17 +3,26 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="group1", header="groupHeader1", footer="GroupFooter1"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_group_with_long_name(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group
                      (name="group1 test testgroup1 test testgroup1 test testgroup1 test testgroup1 test testgroup1 "
                            "test testgroup1 test testgroup1 test testgroup1 test testgroup1 test testgroup1 test "
                            "testgroup1 test testgroup1 test testgroup1 test testgroup1 test testgroup1 test tes",
                       header="Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)Group header header header header header header (Logo)",
                       footer="Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)Group footer (Comment)"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
--- test/Group/test_edit_group.py
+++ test/Group/test_edit_group.py
@@ -2,30 +2,45 @@ from model.group import Group
 
 
 def test_edit_first_group(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name='group is not created'))
     app.group.edit_first_group(Group(name="Edit Group", header="Edit Header", footer="Edit GroupFooter"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_first_group_to_empty_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name='group is not created'))
     app.group.edit_first_group(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_group_name_first_group(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name='group is not created'))
     app.group.edit_first_group(Group(name="Edit ONLY Group name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_group_header_first_group(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name='group is not created'))
     app.group.edit_first_group(Group(header="Edit ONLY Group Header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_group_footer_first_group(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name='group is not created'))
     app.group.edit_first_group(Group(footer="Edit ONLY Group Footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
