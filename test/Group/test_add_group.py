90ab62497a114f94e8e08714ca1b010d72ac39a9
Rymarava Natalia <rymareva.nat@gmail.com>
2022-04-25 18:57:58 +0300

    Task 29: implemented Allure report

--- test/Group/test_add_group.py
+++ test/Group/test_add_group.py
@@ -4,7 +4,6 @@ import allure
 from model.group import Group
 
 
-
 def test_add_group(app, db, json_groups):
     group = json_groups
     with allure.step('Given a group list'):
@@ -15,5 +14,3 @@ def test_add_group(app, db, json_groups):
         new_groups = db.get_group_list()
         old_groups.append(group)
         assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
